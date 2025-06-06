dcf4b1c32421da400b981d97fee6e133253edcb8
arda-atalik <116770567+arda-atalik@users.noreply.github.com>
2025-06-05 18:39:14 -0700

    Update main_m4.py

--- scripts/main_m4.py
+++ scripts/main_m4.py
@@ -2,13 +2,13 @@ import numpy as np
 import torch
 import argparse
 
-from distributed import init_distributed_mode
 from torch import optim, nn
 from fastmri.data.subsample import EquiSpacedMaskFunc
 from fastmri.data.transforms import center_crop_to_smallest
 from tgvn.data import VarNetDataTransformM4Joint, SliceDatasetM4Joint
 from tgvn.models import VarNetImage, TGVN_2S
 from tgvn.loss import MS_SSIM_L1Loss
+from tgvn.distributed import init_distributed_mode
 from fastmri import SSIMLoss
 
 
